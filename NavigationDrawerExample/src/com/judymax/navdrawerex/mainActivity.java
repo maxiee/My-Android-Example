1f347134843e9adc1e0b408852563703a1ecff82
Maxiee <maxieewong@gmail.com>
2014-08-29 08:51:52 +0000

    finish NavigationDrawerExample

--- NavigationDrawerExample/src/com/judymax/navdrawerex/mainActivity.java
+++ NavigationDrawerExample/src/com/judymax/navdrawerex/mainActivity.java
@@ -1,15 +1,78 @@
 package com.judymax.navdrawerex;
 
+import com.judymax.navdrawerex.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.res.Configuration;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 
 public class mainActivity extends Activity
 {
-    /** Called when the activity is first created. */
+    private String[] drawerListViewItems;
+    private ListView drawerListView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        drawerListViewItems = getResources().getStringArray(R.array.items);
+        drawerListView = (ListView)findViewById(R.id.left_drawer);
+
+        drawerListView.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_listview_item, drawerListViewItems));
+        drawerListView.setOnItemClickListener(new DrawerItemClickListener());
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+                );
+        
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow,GravityCompat.START);
+    }
+
+    @Override
+    public void onConfigurationChanged( Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id){
+            Toast.makeText(mainActivity.this, ((TextView)view).getText(), Toast.LENGTH_LONG).show();
+            drawerLayout.closeDrawer(drawerListView);
+        }
     }
 }
